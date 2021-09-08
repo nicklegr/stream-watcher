1c69d3bf340506f626c21744941b594f4d760d70
nicklegr <1327502+nicklegr@users.noreply.github.com>
2021-09-09 00:07:12 +0900

    リファクタ

--- web.rb
+++ web.rb
@@ -70,9 +70,7 @@ post "/api/v1/twitter_space/bulk_check" do
   end
 
   # 複数のユーザーが同じスペースにいる場合があるのでuniq
-  spaces.uniq!{|e| e[:spaces][:live_content][:audiospace][:broadcast_id]}
-
-  space_ids = spaces.map{|e| e[:spaces][:live_content][:audiospace][:broadcast_id]}
+  space_ids = spaces.map{|e| e[:spaces][:live_content][:audiospace][:broadcast_id]}.uniq
 
   results = space_ids.map do |space_id|
     audio_space = space.audio_space_by_id(token, space_id)
